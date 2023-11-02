ed1fce4f58130fe4bb46bfc3a642668b07db0f97
GustaVasconcelos <gustavo.vasconcelos.peixoto18@gmail.com>
2023-11-02 10:17:32 -0300

    feat:adicionando fundamentos

--- arquitetura_hexagonal/src/app/fundamentos/polimorfismo.ts
+++ arquitetura_hexagonal/src/app/fundamentos/polimorfismo.ts
@@ -1,13 +1,38 @@
+import Carro from "@/core/fundamentos/carro";
 import TerminalUtil from "../util/terminalUtil"
+import Ferrari from "@/core/fundamentos/ferrari";
+import Fusca from "@/core/fundamentos/fusca";
 
 const polimorfismo = async () => {
     TerminalUtil.titulo("Polimorfismo");
 
-    const tipoCarro = await TerminalUtil.selecao('Tipo de carro', ['Ferrari', 'Fusca']);
+    const [tipoCarro] = await TerminalUtil.selecao('Tipo de carro', ['Ferrari', 'Fusca']);
+    
+    const carro: Carro = tipoCarro === 0? new Ferrari() : new Fusca();
     
     while(true) {
         TerminalUtil.limpar();
 
+        TerminalUtil.exibirChaveValor(
+            'Velocidade Máxima: ', 
+            `${carro.velocidadeMaxima} km/h`
+        );
+
+        TerminalUtil.exibirChaveValor(
+            'Velocidade Atual: ', 
+            `${carro.velocidadeAtual} km/h`
+        );
+
+        const [opcao] = await TerminalUtil.selecao(
+            "Qual opção?",
+            [
+                "Acelerar",
+                "Frear"
+            ]
+        )
+
+        opcao === 0 ? carro.acelerar() : carro.frear();
+        
         const continuar =  await TerminalUtil.confirmacao('Deseja continuar?');
     
         if (!continuar) return;
